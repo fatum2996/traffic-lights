d4397dd37384de6a7af1bbf0f775ad40ce9e725d
Анисимов Юрий <YAnisimov@ask.ru>
2017-04-28 10:00:44 +0300

    Added second traffic lights

--- traffic-lights.c
+++ traffic-lights.c
@@ -2,56 +2,72 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #define F_CPU 8000000UL
-#define yellow_time 2
-#define seconds_big 14
-#define seconds_little 7
-#define blink_time 3
+#define yellow_time_big 6
+#define yellow_time_little 3
+#define time_big 28
+#define time_little 14
+
 
 int main(void)
 {
-	char seconds = 0;
+	char time = 0;
 	char i;
 	cli();
-	DDRB = 0b00011100;
+	DDRB = 0b11111100;
 	DDRD = 0b0000000;
 	PORTD |= 1 << PD2;
+	char greentime = 0;
+	char yellowtime = 0;
 	
-	i = PIND;
-	if(i & (1<<PD2))
-		seconds = seconds_little;
-	else
-		seconds = seconds_big;
 	while(1) {
 		i = PIND;
-		if(i & (1<<PD2))
-			seconds = seconds_little;
-		else
-			seconds = seconds_big;
-		PORTB |= 1 << PB2; //зажечь красный
-		for(i = 0; i < seconds; i++) {
-			_delay_ms(250);
-			_delay_ms(250);
-			_delay_ms(250);
-			_delay_ms(250);
+		if(i & (1<<PD2)) {
+			time = time_big;
+			yellowtime = yellow_time_big; 
 		}
-		PORTB &= ~(1<< PB2); //гасим красный
-		PORTB |= 1 << PB3; //зажечь желтый
-		for(i = 0; i < yellow_time; i++) {
-			_delay_ms(250);
-			_delay_ms(250);
-			_delay_ms(250);
-			_delay_ms(250);
+		else {
+			time = time_little;
+			yellowtime = yellow_time_little; 
 		}
-		PORTB &= ~(1<< PB3); //гасим желтый
+		greentime = time - 2 * yellowtime;
 
-		PORTB |= 1 << PB4; //зажечь зеленый
-		for(i = 0; i < seconds-blink_time; i++) {
-			_delay_ms(250);
-			_delay_ms(250);
+		PORTB |= 1 << PB2; //зажечь 1 красный
+
+		PORTB |= 1 << PB7; //зажечь 2 зеленый
+
+		for(i = 0; i < greentime; i++) {
 			_delay_ms(250);
 			_delay_ms(250);
 		}
-		for(i=0; i < blink_time; i++) { //мигаем 3 с
+
+		for(i=0; i < 3; i++) { //мигаем 2 зел 3 с
+			PORTB |= 1 << PB7;
+			_delay_ms(250);
+			_delay_ms(250);	
+			PORTB &= ~(1<< PB7);
+			_delay_ms(250);	
+			_delay_ms(250);			
+		}
+
+		PORTB |= 1 << PB6; //зажечь 2 желтый
+		for(i = 0; i < yellowtime; i++) {
+			_delay_ms(250);
+			_delay_ms(250);
+		}
+
+		PORTB &= ~(1<< PB2); //гасим 1 красный
+
+		PORTB &= ~(1<< PB6); //гасим 2 желтый
+
+		PORTB |= 1 << PB5; //зажечь 2 красный
+
+		PORTB |= 1 << PB4; //зажечь 1 зеленый
+		for(i = 0; i < greentime; i++) {
+			_delay_ms(250);
+			_delay_ms(250);
+		}
+
+		for(i=0; i < 3; i++) { //мигаем 3 с
 			PORTB |= 1 << PB4;
 			_delay_ms(250);
 			_delay_ms(250);	
@@ -59,14 +75,16 @@ int main(void)
 			_delay_ms(250);	
 			_delay_ms(250);			
 		}
-		PORTB |= 1 << PB3; //зажечь желтый
-		for(i = 0; i < yellow_time; i++) {
-			_delay_ms(250);
-			_delay_ms(250);
+
+		PORTB |= 1 << PB3; //зажечь 1 желтый
+		for(i = 0; i < yellowtime; i++) {
 			_delay_ms(250);
 			_delay_ms(250);
 		}
-		PORTB &= ~(1<< PB3); //гасим желтый
+
+		PORTB &= ~(1<< PB3); //гасим 1 желтый
+
+		PORTB &= ~(1<< PB5); //гасим 2 красный
 	}
 
 }
